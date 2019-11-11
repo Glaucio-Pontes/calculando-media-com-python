3d0d0dcb775a7a797980fede3732d670f6659d56
glaucio <pontesglaucio16@gmail.com>
2019-11-11 02:00:54 -0300

    treinando estatistica com python

---
+++ calculo_media.py
@@ -0,0 +1,7 @@
+valor1 = int(input("Qual o primeiro valor?"))
+valor2 = int(input("Qual o segundo valor?"))
+valor3 = int(input("Qual o terceiro valor?"))
+
+media = (valor1 + valor2 + valor3)/ 3
+
+print(media)
